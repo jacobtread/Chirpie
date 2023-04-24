86981878403b9046d0e50753f40226a0213508a7
Jacobtread <jacobtread@gmail.com>
2023-04-25 11:05:10 +1200

    Setup frontend for auth0 and dependencies for backend and small frontend configs

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+// Environment variable IntelliSense
+interface ImportMetaEnv {
+    readonly VITE_AUTH0_DOMAIN: string
+    readonly VITE_AUTH0_CLIENT_ID: string
+}
