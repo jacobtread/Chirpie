f028f13147f36862a0fd4df607e11fd1dddd65d6
Jacobtread <jacobtread@gmail.com>
2023-04-25 11:40:41 +1200

    Created database structures and connect fn

---
+++ backend/src/database.rs
@@ -0,0 +1,70 @@
+use std::process::exit;
+
+use chrono::NaiveDateTime;
+use log::error;
+use mongodb::{bson::oid::ObjectId, options::ClientOptions, Client, Database};
+use serde::{Deserialize, Serialize};
+
+/// Structure of a user within the app database
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    /// Unique ID for the user account
+    pub id: ObjectId,
+    /// Unique username of the user
+    pub username: String,
+    /// Email address of the user
+    pub email: String,
+}
+
+/// Structure of a chirp (Tweet) message
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Chirp {
+    /// Unique ID for the chirp
+    pub id: ObjectId,
+    /// ID of the user that made the chirp
+    pub owner: ObjectId,
+    /// The text of the chirp
+    pub text: String,
+    /// Creation date and time
+    pub created_at: NaiveDateTime,
+}
+
+const DATABASE_NAME: &str = "chirpie";
+const DATABASE_URL: &str = "DATABASE_URL";
+
+/// Connects to the MongoDB database returning a database connection. If there
+/// is an error during connection the process will exit with an error message1
+pub async fn connect() -> Database {
+    // Obtain database url
+    let database_url = match std::env::var(DATABASE_URL) {
+        Ok(value) => value,
+        Err(_) => {
+            error!(
+                "Missing database url environment variable: {}",
+                DATABASE_URL
+            );
+            exit(1);
+        }
+    };
+
+    // Setup client options
+    let mut options = match ClientOptions::parse(database_url).await {
+        Ok(value) => value,
+        Err(err) => {
+            error!("Failed to parse database options: {}", err);
+            exit(1);
+        }
+    };
+    options.app_name = Some("Chirpie".to_string());
+
+    // Create client
+    let client = match Client::with_options(options) {
+        Ok(value) => value,
+        Err(err) => {
+            error!("Failed to connect to database: {}", err);
+            exit(1);
+        }
+    };
+
+    client.database(DATABASE_NAME)
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,10 @@
+use dotenvy::dotenv;
+
+mod database;
+
 fn main() {
+    // Load environment variables
+    let _ = dotenv();
+
     println!("Hello, world!");
 }
